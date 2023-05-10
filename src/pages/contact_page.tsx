0552097a966c2804e7e6e5a8a71150fc80ac1818
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-05-10 23:43:14 +0200

    update

--- src/pages/contact_page.tsx
+++ src/pages/contact_page.tsx
@@ -1,40 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import io, { Socket } from 'socket.io-client';
 
 type ContactPageProps = {
   isDark: boolean;
 };
 
 const ContactPage: React.FC<ContactPageProps> = ({ isDark }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [response, setResponse] = useState<string>('');
+
+  useEffect(() => {
+    const newSocket = io('http://18.200.219.78:80/');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData: Record<string, string> = {};
+
+    const formElements = e.currentTarget.elements as HTMLFormControlsCollection;
+    
+    for (let i = 0; i < formElements.length; i++) {
+      const element = formElements[i] as (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement);
+      if (element.name && element.value) {
+        formData[element.name] = element.value;
+      }
+    }
+
+    if (socket) {
+      socket.emit('contact', formData, (response: string) => {
+        setResponse(response);
+      });
+    }
+  };
+
   return (
-    <div className='main2' style={{ backgroundImage: isDark ? 'radial-gradient(#ffffff 1px, #000000 1px)' 
-    :
-     'radial-gradient(#000000 1px, #ffffff 1px)' }}>
-      
-      
-
-
-      
-      <table className='contact_form'  style={{ backgroundColor: isDark ? '#000' : '#fff' , border: isDark ? '1px solid white' : '1px solid black'}}>
-  <tbody>
-    <tr>
-      <td><br /><input style={{ backgroundColor: isDark ? '#000' : '#fff' , color: isDark ? '#fff' : '#000' , borderBottom: isDark ? '1px solid white' : '1px solid black'}} type="text" placeholder="Name"/></td>
-    </tr>
-    <tr>
-      <td><input style={{ backgroundColor: isDark ? '#000' : '#fff' , color: isDark ? '#fff' : '#000' , borderBottom: isDark ? '1px solid white' : '1px solid black'}}  type="email" placeholder="Email"/></td>
-    </tr>
-    <tr>
-      <td><textarea style={{ backgroundColor: isDark ? '#000' : '#fff' , color: isDark ? '#fff' : '#000' , borderBottom: isDark ? '1px solid white' : '1px solid black'}}  placeholder="Message"></textarea></td>
-    </tr>
-    <tr>
-      <td><button style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>Contact Us</button><br /></td>
-    </tr>
-  </tbody>
-</table>
-
-
-      
-       </div>
+    <div className="main" style={{ backgroundImage: isDark ? 'radial-gradient(#ffffff 1px, #000000 1px)' : 'radial-gradient(#000000 1px, #ffffff 1px)' }}>
+      <h1>Contact Us</h1>
+      <p>Get in touch with us by filling in the form below:</p>
+      <form onSubmit={handleSubmit} className="contact-form" style={{ backgroundColor: isDark ? '#000' : '#fff', border: isDark ? '1px solid white' : '1px solid black' }}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" placeholder="Your name here" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" placeholder="Your email address" />
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" placeholder="Type your message here"></textarea>
+        <button type="submit">Send Message</button>
+      </form>
+      {response && <div>{response}</div>}
+    </div>
   );
 };
 
